8cc35206374943b05f93adb915828cee4b2d0e50
cunhaluisg5 <cunhaluisg5@gmail.com>
2018-09-19 13:00:54 -0300

    Implementada classe UsaQuadrado

--- src/exercicio_2/UsaQuadrado.java
+++ src/exercicio_2/UsaQuadrado.java
@@ -5,6 +5,8 @@
  */
 package exercicio_2;
 
+import model.Quadrado;
+
 /**
  *
  * @author Luis
@@ -15,7 +17,18 @@ public class UsaQuadrado {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Quadrado quadrado1 = new Quadrado(4);
+        Quadrado quadrado2 = new Quadrado(2);
+        Quadrado quadrado3 = new Quadrado(6);
+        
+        System.out.println("A área do primeiro quadrado é " + quadrado1.calcularArea() + 
+        " e seu perímetro é " + quadrado1.calcularPerimetro());
+        
+        System.out.println("A área do segundo quadrado é " + quadrado2.calcularArea() + 
+        " e seu perímetro é " + quadrado2.calcularPerimetro());
+        
+        System.out.println("A área do terceiro quadrado é " + quadrado3.calcularArea() + 
+        " e seu perímetro é " + quadrado3.calcularPerimetro());
     }
     
 }
